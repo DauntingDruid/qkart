b6326a1eace92ff7aeaf6938f0a13bc313a3c9fe
Abhishek <abhishekroxtomar@gmail.com>
2023-07-12 13:34:39 +0530

    backend api updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Checkout from './components/Checkout';
 import Thanks from './components/Thanks';
 
 export const config = {
-  endpoint: `http://localhost:8082/api/v1`,
+  endpoint: `https://qkart-backend-dauntingdruid.onrender.com/api/v1`,
 };
 
 function App() {
